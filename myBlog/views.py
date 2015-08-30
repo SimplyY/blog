e3e3b14b0824b3d1ff3b8c1d3d85e76622df4023
SimplyY <SimplyYu@163.com>
2015-08-30 23:14:49 +0800

    add asidebar

--- myBlog/views.py
+++ myBlog/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.template import Context
 from myBlog.models import *
+from urllib.parse import unquote
 
 def root(request):
     return redirect('/home')
 
 
 def home_page(request):
-    current_tags = [Tag.objects.filter()]
+    current_tags = Tag.objects.filter(name='编程')
+    print(current_tags)
     return render_to_response('home.html', Context({"article_list": Article.objects.all(), "current_tags": current_tags}))
 
 
 def tag_page(request):
     url = request.get_full_path()
-    tag_name = url.split('/')[-1]
+    tag_name = unquote(url.split('/')[-1])
 
     article_list = []
     current_tags = []
 
     for article in Article.objects.all():
+        print(article.tag.name)
         if article.tag.name == tag_name:
             current_tags = article.tag.get_tags()
             article_list.append(article)
