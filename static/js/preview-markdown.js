3869d74fba29a53169e411cd16b0511ae4b2a6c7
SimplyY <SimplyYu@163.com>
2015-09-23 09:16:07 +0800

    debug about unescapeHTML

--- static/js/preview-markdown.js
+++ static/js/preview-markdown.js
@@ -1,2 +1,27 @@
-var preview = document.getElementById("article-md-preview");
-preview.innerHTML = markdown.toHTML(preview.innerHTML);
+(function (document) {
+    var preview = document.getElementById("article-md-preview");
+    preview = (preview);
+
+    console.log("unescapeHTML(preview.innerHTML)");
+    preview.innerHTML = markdown.toHTML(unescapeHTML(preview.innerHTML));
+
+    /**
+     * @function escapeHTML 转义html脚本 < > & " '
+     * @param a -
+     *            字符串
+     */
+    function escapeHTML(a){
+    	a = "" + a;
+    	return a.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+    }
+    /**
+     * @function unescapeHTML 还原html脚本 < > & " '
+     * @param a -
+     *            字符串
+     */
+    function unescapeHTML(a){
+    	a = "" + a;
+    	return a.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&apos;/g, "'");
+    }
+
+})(document);
