1a9f3311f3b31b90cf60c8be6621a5c232edd59f
SimplyY <simplyyu@163.com>
2015-10-01 10:20:48 +0800

    fix url bug

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from myBlog.views import *
 
 urlpatterns = [
-    url(r'^$', home_page),
-    url(r'^admin', include(admin.site.urls)),
-    url(r'^home$', home_page),
-    url(r'^tag/', tag_page, name='tags'),
-    url(r'^article/([0-9]+)$', article_page, name='articles'),
-    url(r'^renew_article$', renew_article),
-    url(r'^author$', author_page),
+    url(r'^blog$', home_page),
+    url(r'^blog/admin', include(admin.site.urls)),
+    url(r'^blog/home$', home_page),
+    url(r'^blog/tag/', tag_page, name='tags'),
+    url(r'^blog/article/([0-9]+)$', article_page, name='articles'),
+    url(r'^blog/renew_article$', renew_article),
+    url(r'^blog/author$', author_page),
 ]
