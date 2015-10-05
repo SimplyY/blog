0fe4c0d923a3d1e10eb25b4f82602e40f7b89a3b
SimplyY <SimplyYu@163.com>
2015-10-05 10:32:34 +0800

    change author page 's love-info's seconds

--- static/js/tuodan.js
+++ static/js/tuodan.js
@@ -5,12 +5,22 @@
     var seconds =  Math.floor((Date.now()-days*1000*3600*24-Date.UTC(2014, 6, 14, 2))/1000);
 
     $(".lovedays")[0].innerHTML= days.toString() + "天";
-    var secondsHtml = $(".seconds")[0];
-    secondsHtml.innerHTML = seconds.toString() + "秒";
+    var secondsDOM = $(".seconds")[0];
+    secondsDOM.innerHTML = seconds.toString() + "秒";
 
     var cycleTime = 1000;
     setTimeout(function(){
-        secondsHtml.innerHTML = (seconds+1).toString() + "秒";
+        seconds += 1;
+
+        var loveInfo = $("#love-info")[0];
+        loveInfo.removeChild($(".seconds")[0]);
+
+        var newSecondsDOM = document.createElement('p');
+        newSecondsDOM.innerHTML = seconds.toString() + "秒";
+        loveInfo.appendChild(newSecondsDOM);
+        $('p')[3].setAttribute('id', 'seconds');
+        $('#seconds')[0].setAttribute('class', 'seconds');
+
         setTimeout(arguments.callee, cycleTime);
     }, cycleTime);
 
