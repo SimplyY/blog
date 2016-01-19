df1fe6ce6884a8ed139ae3863accd029ae916fff
SimplyY <SimplyYu@163.com>
2016-01-19 15:50:41 +0800

    change server prot in config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 module.exports = {
     blogRootPath: "/Users/yuwei/GitHub/Blog/Article",
     mongoUrl: 'mongodb://127.0.0.1:27017/blog',
-    serverPort: '5000',
+    serverPort: '80',
     tokenExpireTime: 10 // x hours
 };
