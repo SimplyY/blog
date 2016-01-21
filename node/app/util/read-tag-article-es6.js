3eb8ead70c921989bdd70e99f32de3e1460bd738
SimplyY <SimplyYu@163.com>
2016-01-21 17:44:32 +0800

    change read-tag-article export method

--- node/app/util/read-tag-article-es6.js
+++ node/app/util/read-tag-article-es6.js
@@ -2,55 +2,28 @@ import fse from 'fs-extra';
 import { basename, dirname, extname } from 'path';
 import { readFile } from 'fs';
 
-let tags = [];
-let articles = [];
+export { getTagAndArticle };
 
-var config = require('../config');
-const ROOT_DIR = config.blogRootPath;
 
-fse.walk(ROOT_DIR)
-    .on('data', function (item) {
-        if (item.stats.isDirectory()) {
-            // create tag and add in tags
-            let tag = createTag(item.path);
-            if (tag !== undefined) {
-                tags.push(tag);
-            }
-        }
+function getTagAndArticle(callback) {
 
-        if (item.stats.isFile()) {
-            // create article and add in articles
-            if (extname(item.path) !== '.md') {
-                return;
-            }
+    let tags = [];
+    let articles = [];
 
-            let title = getTile(item.path);
-            let parentTagName = basename(dirname(item.path));
-            readFile(item.path, 'utf8', function (err, data) {
-                if (err) throw err;
-                let md = data;
-                let parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, item.path);
-                let article = { title, md, parentTagName, parentsTagNameArray };
-                articles.push(article);
-            });
-        }
-    });
+    var config = require('../../../config');
+    const ROOT_DIR = config.blogRootPath;
 
-function createTag(path) {
-    let tagName = basename(path);
-    let parentTagName = basename(dirname(path));
-    // 根目录不作为 tag
-    if (basename(ROOT_DIR) === tagName) {
-        return;
-    }
-    // 使第一级 tag 的父目录为空
-    if (parentTagName === ROOT_DIR) {
-        parentTagName = "";
-    }
-    let parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, path);
-    let aritcleTitleList = [];
-    fse.walk(path)
+    fse.walk(ROOT_DIR)
         .on('data', function (item) {
+            if (item.stats.isDirectory()) {
+                // create tag and add in tags
+
+                let tag = createTag(item.path);
+                if (tag !== undefined) {
+                    tags.push(tag);
+                }
+            }
+
             if (item.stats.isFile()) {
                 // create article and add in articles
                 if (extname(item.path) !== '.md') {
@@ -58,25 +31,63 @@ function createTag(path) {
                 }
 
                 let title = getTile(item.path);
-                aritcleTitleList.push(title);
+                let parentTagName = basename(dirname(item.path));
+                readFile(item.path, 'utf8', function (err, data) {
+                    if (err) throw err;
+                    let md = data;
+                    let parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, item.path);
+                    let article = { title, md, parentTagName, parentsTagNameArray };
+                    articles.push(article);
+                });
             }
+        })
+        .on('end', function () {
+            callback(tags, articles);
         });
 
-    return { tagName, parentTagName, aritcleTitleList, parentsTagNameArray };
-}
+    function createTag(path) {
+        let tagName = basename(path);
+        let parentTagName = basename(dirname(path));
+        // 根目录不作为 tag
+        if (basename(ROOT_DIR) === tagName) {
+            return;
+        }
+        // 使第一级 tag 的父目录为空
+        if (parentTagName === ROOT_DIR) {
+            parentTagName = "";
+        }
+        let parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, path);
+        let aritcleTitleList = [];
+        fse.walk(path)
+            .on('data', function (item) {
+                if (item.stats.isFile()) {
+                    // create article and add in articles
+                    if (extname(item.path) !== '.md') {
+                        return;
+                    }
 
-function getParentsTagNameArray(ROOT_DIR, path) {
-    var array = [];
-    let parentPath = dirname(path);
-    while (parentPath !== ROOT_DIR) {
-        array.unshift(basename(parentPath));
-        parentPath = dirname(parentPath);
+                    let title = getTile(item.path);
+                    aritcleTitleList.push(title);
+                }
+            });
+
+        return { tagName, parentTagName, aritcleTitleList, parentsTagNameArray };
     }
-    return array;
-}
 
-function getTile(baseName) {
-     return basename(baseName, '.md');
-}
+    function getParentsTagNameArray(ROOT_DIR, path) {
+        var array = [];
+        let parentPath = dirname(path);
+        while (parentPath !== ROOT_DIR) {
+            array.unshift(basename(parentPath));
+            parentPath = dirname(parentPath);
+        }
+        return array;
+    }
 
-export {articles, tags};
+    function getTile(baseName) {
+         return basename(baseName, '.md');
+    }
+
+
+
+}
--- node/app/util/read-tag-article.js
+++ node/app/util/read-tag-article.js
@@ -5,7 +5,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.tags = exports.articles = undefined;
+exports.getTagAndArticle = undefined;
 
 var _fsExtra = require('fs-extra');
 
@@ -17,86 +17,91 @@ var _fs = require('fs');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var tags = [];
-var articles = [];
+exports.getTagAndArticle = getTagAndArticle;
 
-var config = require('../config');
-var ROOT_DIR = config.blogRootPath;
+function getTagAndArticle(callback) {
 
-_fsExtra2.default.walk(ROOT_DIR).on('data', function (item) {
-    if (item.stats.isDirectory()) {
-        // create tag and add in tags
-        var tag = createTag(item.path);
-        if (tag !== undefined) {
-            tags.push(tag);
-        }
-    }
+    var tags = [];
+    var articles = [];
 
-    if (item.stats.isFile()) {
-        var _ret = function () {
-            // create article and add in articles
-            if ((0, _path.extname)(item.path) !== '.md') {
-                return {
-                    v: undefined
-                };
+    var config = require('../../../config');
+    var ROOT_DIR = config.blogRootPath;
+
+    _fsExtra2.default.walk(ROOT_DIR).on('data', function (item) {
+        if (item.stats.isDirectory()) {
+            // create tag and add in tags
+
+            var tag = createTag(item.path);
+            if (tag !== undefined) {
+                tags.push(tag);
             }
+        }
 
-            var title = getTile(item.path);
-            var parentTagName = (0, _path.basename)((0, _path.dirname)(item.path));
-            (0, _fs.readFile)(item.path, 'utf8', function (err, data) {
-                if (err) throw err;
-                var md = data;
-                var parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, item.path);
-                var article = { title: title, md: md, parentTagName: parentTagName, parentsTagNameArray: parentsTagNameArray };
-                articles.push(article);
-            });
-        }();
-
-        if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
-    }
-});
-
-function createTag(path) {
-    var tagName = (0, _path.basename)(path);
-    var parentTagName = (0, _path.basename)((0, _path.dirname)(path));
-    // 根目录不作为 tag
-    if ((0, _path.basename)(ROOT_DIR) === tagName) {
-        return;
-    }
-    // 使第一级 tag 的父目录为空
-    if (parentTagName === ROOT_DIR) {
-        parentTagName = "";
-    }
-    var parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, path);
-    var aritcleTitleList = [];
-    _fsExtra2.default.walk(path).on('data', function (item) {
         if (item.stats.isFile()) {
-            // create article and add in articles
-            if ((0, _path.extname)(item.path) !== '.md') {
-                return;
-            }
+            var _ret = function () {
+                // create article and add in articles
+                if ((0, _path.extname)(item.path) !== '.md') {
+                    return {
+                        v: undefined
+                    };
+                }
 
-            var title = getTile(item.path);
-            aritcleTitleList.push(title);
+                var title = getTile(item.path);
+                var parentTagName = (0, _path.basename)((0, _path.dirname)(item.path));
+                (0, _fs.readFile)(item.path, 'utf8', function (err, data) {
+                    if (err) throw err;
+                    var md = data;
+                    var parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, item.path);
+                    var article = { title: title, md: md, parentTagName: parentTagName, parentsTagNameArray: parentsTagNameArray };
+                    articles.push(article);
+                });
+            }();
+
+            if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
         }
+    }).on('end', function () {
+        callback(tags, articles);
     });
 
-    return { tagName: tagName, parentTagName: parentTagName, aritcleTitleList: aritcleTitleList, parentsTagNameArray: parentsTagNameArray };
-}
+    function createTag(path) {
+        var tagName = (0, _path.basename)(path);
+        var parentTagName = (0, _path.basename)((0, _path.dirname)(path));
+        // 根目录不作为 tag
+        if ((0, _path.basename)(ROOT_DIR) === tagName) {
+            return;
+        }
+        // 使第一级 tag 的父目录为空
+        if (parentTagName === ROOT_DIR) {
+            parentTagName = "";
+        }
+        var parentsTagNameArray = getParentsTagNameArray(ROOT_DIR, path);
+        var aritcleTitleList = [];
+        _fsExtra2.default.walk(path).on('data', function (item) {
+            if (item.stats.isFile()) {
+                // create article and add in articles
+                if ((0, _path.extname)(item.path) !== '.md') {
+                    return;
+                }
 
-function getParentsTagNameArray(ROOT_DIR, path) {
-    var array = [];
-    var parentPath = (0, _path.dirname)(path);
-    while (parentPath !== ROOT_DIR) {
-        array.unshift((0, _path.basename)(parentPath));
-        parentPath = (0, _path.dirname)(parentPath);
+                var title = getTile(item.path);
+                aritcleTitleList.push(title);
+            }
+        });
+
+        return { tagName: tagName, parentTagName: parentTagName, aritcleTitleList: aritcleTitleList, parentsTagNameArray: parentsTagNameArray };
     }
-    return array;
-}
 
-function getTile(baseName) {
-    return (0, _path.basename)(baseName, '.md');
-}
+    function getParentsTagNameArray(ROOT_DIR, path) {
+        var array = [];
+        var parentPath = (0, _path.dirname)(path);
+        while (parentPath !== ROOT_DIR) {
+            array.unshift((0, _path.basename)(parentPath));
+            parentPath = (0, _path.dirname)(parentPath);
+        }
+        return array;
+    }
 
-exports.articles = articles;
-exports.tags = tags;
+    function getTile(baseName) {
+        return (0, _path.basename)(baseName, '.md');
+    }
+}
