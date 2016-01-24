1425f30fc84c28c8482c9f13d2e0614795f97593
SimplyY <SimplyYu@163.com>
2016-01-24 10:52:40 +0800

    auth mongodb

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     blogRootPath: "/Users/yuwei/GitHub/Blog/Article",
-    mongoUrl: 'mongodb://127.0.0.1:27017/blog',
+    mongoUrl: 'mongodb://simplyy:1994522yu@127.0.0.1:27017/blog',
     serverPort: '5000',
     renewInterval: 60, // x seconds
     tokenExpireTime: 10 // x hours
