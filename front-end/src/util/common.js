531dd8b80f56d525b0de986a9dda93881e6da4d3
SimplyY <SimplyYu@163.com>
2016-02-13 23:52:05 +0800

    fix anchor bug

--- front-end/src/util/common.js
+++ front-end/src/util/common.js
@@ -1,5 +1,11 @@
 export function anchorHandler(e) {
-    window.scrollTo(0, e.target.offsetTop - 80)
     e.preventDefault()
+    let anchorName = e.target.href.split('#')[1]
+    if (location.href.indexOf('#') === -1) {
+        location.href += '#' + anchorName
+    } else {
+        location.href = location.href.split('#')[0] + '#' + anchorName
+    }
+    window.scrollTo(0, e.target.offsetTop + 30)
     // TODO: highliaht animation
 }
