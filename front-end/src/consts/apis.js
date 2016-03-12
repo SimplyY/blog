3e1f3c3d5f8db40a1f17c1f5e4633ce6a5dd43fd
SimplyY <SimplyYu@163.com>
2016-03-12 13:53:40 +0800

    renew about tag

--- front-end/src/consts/apis.js
+++ front-end/src/consts/apis.js
@@ -1,3 +1,3 @@
-export const API_ROOT_URL = '//121.40.224.83:80/api/'
+export const API_ROOT_URL = '//121.40.224.83:8000/api/'
 export const TAGS_URL = 'tags/'
 export const ARTICLES_URL = 'articles/'
