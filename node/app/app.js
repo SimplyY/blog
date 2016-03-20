6a35e4a7f1f6616d0921fcde5489e4f3e870a100
SimplyY <SimplyYu@163.com>
2016-03-20 10:02:59 +0800

    disable cors

--- node/app/app.js
+++ node/app/app.js
@@ -21,7 +21,7 @@ console.log('listening:', config.serverPort);
 
 function setHeader(app) {
     app.all('*', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "http://simplyy.space");
         res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
         res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
         if (req.method == 'OPTIONS') res.sendStatus(200);
